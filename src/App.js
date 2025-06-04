510e917bd49a953d6886d0a86e5b0021957caa6d
Shaurya Agarwal <shauryaagarwal050106@gmail.com>
2025-06-04 14:45:39 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">
-      Navbar
+      Saregama
     </a>
     <button
       className="navbar-toggler"
